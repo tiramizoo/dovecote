bc6cc43931a9fc375aee60255adf2b3f42595587
Tomasz Mazur <defkode@gmail.com>
2017-03-14 14:09:26 +0100

    udpate schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -12,20 +12,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140730143530) do
+  create_table :dovecote_messages do |t|
+    t.string :body
+    t.string :originator, null: false
+    t.string :msisdn, null: false
+    t.string :reference
+    t.string :bird_id, null: false
+    t.string :status, null: false
 
-  create_table "dovecote_messages", force: true do |t|
-    t.string   "body"
-    t.string   "originator",         null: false
-    t.string   "recipient",          null: false
-    t.string   "reference"
-    t.string   "bird_id",            null: false
-    t.string   "status",             null: false
-    t.datetime "scheduled_at"
-    t.datetime "datetime"
-    t.datetime "sent_at"
-    t.datetime "buffered_at"
-    t.datetime "delivered_at"
-    t.datetime "delivery_failed_at"
+    t.datetime :scheduled_at
+    t.datetime :sent_at
+    t.datetime :buffered_at
+    t.datetime :delivered_at
+    t.datetime :expired_at
+    t.datetime :delivery_failed_at
+
+    t.timestamps
   end
-
 end
